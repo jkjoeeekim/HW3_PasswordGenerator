8904e477acbccc2d95a717b032bdc2547732ad5f
Master <jkjoeeekim@live.com>
2020-08-23 21:15:04 -0700

    created html and style

---
+++ script.js
@@ -0,0 +1,13 @@
+var lowBox = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var upBox = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var numBox = ["1","2","3","4","5","6","7","8","9","0"]
+var spcBox = ["!","@","#","$","%","^","&","*","?","~"]
+
+function isNumber(evt) {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    return true;
+}
